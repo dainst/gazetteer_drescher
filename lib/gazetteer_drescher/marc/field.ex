b6d543a29c4a22ec5024a2bd201f84091516f8f0
Simon Hohl <mail@dersmon.net>
2016-10-18 18:15:49 +0200

    Working on marc output.

--- lib/gazetteer_drescher/marc/field.ex
+++ lib/gazetteer_drescher/marc/field.ex
@@ -1,3 +1,32 @@
 defmodule GazetteerDrescher.MARC.Field do
-  defstruct []
+  defstruct [:tag, :i1, :i2, subfields: []]
+
+  alias GazetteerDrescher.MARC.Field
+
+  def to_marc(field = %Field{}) do
+    i1 =
+      case field.i1 do
+        nil -> "#"
+        value -> value
+      end
+    i2 =
+      case field.i2 do
+        nil -> "#"
+        value -> value
+      end
+
+    subfields_as_marc = field.subfields
+    |> subfields_to_marc("")
+
+    field_as_marc = i1 <> i2 <> subfields_as_marc
+
+    {field.tag, to_string(field_as_marc)}
+  end
+
+  def subfields_to_marc([], result), do: result
+
+  def subfields_to_marc([{key, value} | tail], result) do
+    result = result <> Atom.to_string(key) <> value <> << 31 >>
+    subfields_to_marc(tail, result)
+  end
 end
--- lib/gazetteer_drescher/marc/record.ex
+++ lib/gazetteer_drescher/marc/record.ex
@@ -1,20 +1,56 @@
 defmodule GazetteerDrescher.MARC.Record do
-  defstruct [:leader, :directory, fields: []]
+  defstruct [:leader, :directory, fields: [], status: :new,
+    force_unicode: false, indicator_count: 2, subfield_code_length: 2,
+    encoding_level: :complete]
 
-  alias GazetteerDrescher.MARC.{Field, Record}
-
-  def new_record() do
-    %Record{}
-  end
-
+  alias GazetteerDrescher.MARC.{ Field, Record }
 
   def add_field(record = %Record{}, field = %Field{}) do
     Map.put(record, :fields, record.fields ++ [field])
   end
 
   def to_marc(record = %Record{}) do
-    # update leader
-    # update directory
-    # condense into string
+    IO.inspect record
+
+    "./test/test.marc"
+    |> Path.dirname
+    |> File.mkdir_p!
+
+    pid = "./test/test.marc"
+    |> File.open!([:write, :utf8])
+
+    processed_fields = Enum.map(record.fields, &Field.to_marc(&1))
+    |> IO.inspect
+
+    directory = processed_fields
+    |> Enum.map(fn({tag, content}) -> {tag, String.length(content)} end )
+    |> Enum.reduce("", &create_dictionary/2)
+
+    fields_final = processed_fields
+    |> Enum.reduce("", &finalize_fields/2)
+
+    directory = directory <> << 30 >>
+
+    marc = directory <> fields_final <> << 29 >>
+
+
+
+    #marc = fields_marc
+    #|>  <> << 29 >>
+
+    IO.write( pid, marc)
+
+  end
+
+  def create_dictionary({tag, length}, result) do
+
+    adjusted_tag = String.pad_leading(to_string(tag), 3, "0")
+    adjusted_length = String.pad_leading(to_string(length), 4, "0")
+
+    result <> adjusted_tag <> adjusted_length
+  end
+
+  def finalize_fields({tag, content}, result) do
+    result <> content
   end
 end
